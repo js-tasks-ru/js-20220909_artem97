e0312d44517e3b9763949185a35ed48fe2711d2b
Artem Pavlov <La.pavlov@mail.ru>
2022-09-21 15:30:50 +0300

    add 3.1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,12 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
-
+export function createGetter(field) {
+  let pathArr = field.split('.');
+  let tempObj;
+  return function getParam(obj, index = 0){
+    tempObj = obj[pathArr[index]]; //стоит в таких случаях делать временную переменную или лучше обращаться каждый раз к переменным объекта?
+    return (typeof(tempObj) === "object")? getParam(tempObj, index + 1) : tempObj;
+  }  
 }
+
